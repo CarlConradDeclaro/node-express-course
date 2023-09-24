6acc57fd30c14f9ce4a23e3e1897e4b426323fe7
carl conrad <carlconrad98071234@gmail.com>
2023-09-24 12:12:05 +0800

    nodes

--- module.js
+++ module.js
@@ -6,7 +6,6 @@ const sayHi = require('./1-util.js')
 const data = require('./3-alternative-flavor.js')
 require('./4-mind-grenade.js')
 
-
 // console.log(data);
 // sayHi(names.edrian);
 // sayHi(names.adolf)
